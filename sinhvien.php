9aa0c30b2c669f5b53528b7eab46495b8fe25954
dongdong22222 <dinhdong123zz@gmail.com>
2024-12-14 12:08:42 +0700

    Thêm file sinhvien.php chứa thông tin sinh viên

---
+++ sinhvien.php
@@ -0,0 +1,11 @@
+<?php
+// Thông tin sinh viên
+$hoten = 'Nguyen Van A';
+$tuoi = 20;
+$lop = 'CNTT';
+
+echo "<h1>Thông Tin Sinh Viên</h1>";
+echo "Họ tên: $hoten<br>";
+echo "Tuổi: $tuoi<br>";
+echo "Lớp: $lop<br>";
+?>
